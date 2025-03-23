931f5de7d8830c5a99d3492846c57dbe4022b89d
Thomas Kam <thomassth@users.noreply.github.com>
2025-03-23 02:34:25 -0400

    name formatting

--- crawling/ttc-subway-closures.py
+++ crawling/ttc-subway-closures.py
@@ -1,10 +1,6 @@
 # %%
 import json
-from bs4 import BeautifulSoup
-from datetime import datetime
-from pathlib import Path
 
-# %%
 # Part 1: retrieve the HTML from the JSON response
 with open('../data/ttc-subway-closures.json') as f:
     data = json.load(f)
@@ -14,6 +10,9 @@ results = data['Results']
 html_parts = list(map(lambda x: x['Html'], results))
 
 # %%
+from bs4 import BeautifulSoup
+from datetime import datetime
+
 # Part 2: data cleanup
 def clean_text(text):
     return text.replace('\xa0', ' ').replace('â€“', '-').replace('\u2013', '-').strip()
@@ -32,18 +31,31 @@ closures = [
     for html in html_parts
 ]
 
-
-
 # %%
+from pathlib import Path
+
 # Part 3: save the data
 
 # make sure the path exist
 path = '../data/ttc/subway-closures'
 Path(path).mkdir(parents=True, exist_ok=True)
 
-# Save each object to a separate JSON file
+closuresByDate = []
+# sort by start_date
+closures_by_start_date = {}
 for closure in closures:
-    filename = f"{closure['start_date']}-{closure['line'].split()[1]}.json"
+    start_date = closure['start_date']
+    if start_date not in closures_by_start_date:
+        closures_by_start_date[start_date] = []
+    closures_by_start_date[start_date].append(closure)
+
+closuresByDate = [closures_by_start_date[date] for date in sorted(closures_by_start_date)]
+
+print(closuresByDate)
+
+# Save each object to a separate JSON file
+for closure in closuresByDate:
+    filename = f"{closure[0]['start_date']}.json"
     filepath = Path(path) / filename
     with open(filepath, 'w') as outfile:
         print('now processing ' + str(filename))
