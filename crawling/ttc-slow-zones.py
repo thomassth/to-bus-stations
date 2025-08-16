a041174dc1708876662e3fc85551ae27c832cfe3
thomassth <thomassth@users.noreply.github.com>
2025-08-16 11:13:48 -0400

    TTC slow zones

---
+++ crawling/ttc-slow-zones.py
@@ -0,0 +1,17 @@
+# %%
+import requests
+import json
+from pathlib import Path
+
+# %%
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64); to-bus-stations'}
+response = requests.get('https://www.ttc.ca/riding-the-ttc/Updates/Reduced-Speed-Zones', headers=headers)
+# make sure the path exist
+path = '../data/ttc'
+Path(path).mkdir(parents=True, exist_ok=True)
+print(response)
+# store the result into a json file.
+with open(path + '/ttc-subway-closure.html', 'w') as outfile:
+    json.dump(response.text, outfile)
+
+
